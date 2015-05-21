b899e41a5c954a5d46753e2e09fb5b2a7188f3b5
Joan Alabort <ja310@imperial.ac.uk>
2015-05-21 16:17:23 +0100

    Update aam.__init__.py

--- menpofit/aam/__init__.py
+++ menpofit/aam/__init__.py
@@ -1,3 +1,11 @@
-from .base import AAM, PatchBasedAAM
-from .builder import AAMBuilder, PatchBasedAAMBuilder
-from .fitter import LucasKanadeAAMFitter
+from .builder import (
+    AAMBuilder, PatchAAMBuilder, LinearAAMBuilder,
+    LinearPatchAAMBuilder, PartsAAMBuilder)
+from .fitter import LKAAMFitter
+from .algorithm import (
+    PFC, PIC,
+    SFC, SIC,
+    AFC, AIC,
+    MAFC, MAIC,
+    WFC, WIC)
+from .result import SerializableAAMFitterResult
