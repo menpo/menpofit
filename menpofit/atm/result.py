ee3b74ba70e0fd7775ed83f3bd03dd294a11f893
Joan Alabort <ja310@imperial.ac.uk>
2015-06-01 14:53:44 +0100

    Update fitter.py and LinearATMAlgorithmResult

--- menpofit/atm/result.py
+++ menpofit/atm/result.py
@@ -12,14 +12,10 @@ class ATMAlgorithmResult(ParametricAlgorithmResult):
 class LinearATMAlgorithmResult(ATMAlgorithmResult):
     r"""
     """
-    def shapes(self, as_points=False):
-        if as_points:
-            return [self.fitter.transform.from_vector(p).sparse_target.points
-                    for p in self.shape_parameters]
-
-        else:
-            return [self.fitter.transform.from_vector(p).sparse_target
-                    for p in self.shape_parameters]
+    @property
+    def shapes(self):
+        return [self.fitter.transform.from_vector(p).sparse_target
+                for p in self.shape_parameters]
 
     @property
     def final_shape(self):
