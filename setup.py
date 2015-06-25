bc72e2d880b2d7ccc8f1f9050dedada9fd5bde8f
James Booth <jabooth@gmail.com>
2015-06-25 16:25:37 +0100

    bump to PEP440 versioneer

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
 from setuptools import setup, find_packages
 import versioneer
 
-project_name = 'menpofit'
 
-# Versioneer allows us to automatically generate versioning from
-# our git tagging system which makes releases simpler.
-versioneer.VCS = 'git'
-versioneer.versionfile_source = '{}/_version.py'.format(project_name)
-versioneer.versionfile_build = '{}/_version.py'.format(project_name)
-versioneer.tag_prefix = 'v'  # tags are like v1.2.0
-versioneer.parentdir_prefix = project_name + '-'  # dirname like 'menpo-v1.2.0'
-
-
-setup(name=project_name,
+setup(name='menpofit',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description="Menpo's image feature point localisation (AAMs, SDMs, "
