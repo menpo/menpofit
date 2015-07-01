ef81fec301da2c03798e6aeb03df64237ce6fce9
Joan Alabort <ja310@imperial.ac.uk>
2015-07-01 18:11:53 +0100

    Add cost for ATMs

--- menpofit/atm/result.py
+++ menpofit/atm/result.py
@@ -7,6 +7,29 @@ from menpofit.result import ParametricAlgorithmResult, MultiFitterResult
 class ATMAlgorithmResult(ParametricAlgorithmResult):
     r"""
     """
+    def __init__(self, image, algorithm, shape_parameters, cost_functions=None,
+                 gt_shape=None):
+        super(ATMAlgorithmResult, self).__init__(
+            image, algorithm, shape_parameters, gt_shape=gt_shape)
+        self._cost_functions = cost_functions
+        self._warped_images = None
+        self._costs = None
+
+    @property
+    def warped_images(self):
+        if self._warped_images is None:
+            self._warped_images = []
+            for p in self.shape_parameters:
+                self.algorithm.transform.from_vector_inplace(p)
+                self._warped_images.append(
+                    self.algorithm.interface.warp(self.image))
+        return self._warped_images
+
+    @property
+    def costs(self):
+        if self._costs is None:
+            self._costs = [f() for f in self._cost_functions]
+        return self._costs
 
 
 # TODO: handle costs!
@@ -33,4 +56,27 @@ class LinearATMAlgorithmResult(ATMAlgorithmResult):
 class ATMFitterResult(MultiFitterResult):
     r"""
     """
-    pass
+    def __init__(self, image, fitter, algorithm_results, affine_correction,
+                 gt_shape=None):
+        super(ATMFitterResult, self).__init__(
+            image, fitter, algorithm_results, affine_correction,
+            gt_shape=gt_shape)
+        self._warped_images = None
+
+    @property
+    def warped_images(self):
+        if self._warped_images is None:
+            algorithm = self.algorithm_results[-1].algorithm
+            self._warped_images = []
+            for s in self.shapes:
+                algorithm.transform.set_target(s)
+                self._warped_images.append(
+                    algorithm.interface.warp(self.image))
+        return self._warped_images
+
+    @property
+    def costs(self):
+        costs = []
+        for a in self.algorithm_results:
+            costs += a.costs
+        return costs
