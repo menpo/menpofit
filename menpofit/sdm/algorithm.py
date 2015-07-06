e96964663cb94a424eaba9ca8e8687d626293967
Joan Alabort <ja310@imperial.ac.uk>
2015-07-06 18:43:23 +0100

    Add new math subpackage
    - At the moment this subpackage contains regression techniques used by sdm.
    - This package might be moved to menpo core in the long run.
    - Fixes small bugs on fitting results and fitters.

--- menpofit/sdm/algorithm.py
+++ menpofit/sdm/algorithm.py
@@ -3,7 +3,8 @@ import numpy as np
 from menpo.feature import no_op
 from menpo.visualize import print_dynamic
 from menpofit.result import NonParametricAlgorithmResult
-
+from menpofit.math import (
+    incremental_least_squares, incremental_indirect_least_squares)
 
 # TODO: compute more meaningful error
 # TODO: document me!
@@ -135,7 +136,7 @@ class Newton(SupervisedDescentAlgorithm):
         self.patch_shape = patch_shape
         self.iterations = iterations
         self.eps = eps
-        self._regressor_cls = _incremental_least_squares
+        self._regressor_cls = incremental_least_squares
 
 
 # TODO: document me!
@@ -149,56 +150,7 @@ class GaussNewton(SupervisedDescentAlgorithm):
         self.patch_shape = patch_shape
         self.iterations = iterations
         self.eps = eps
-        self._regressor_cls = _incremental_indirect_least_squares
-
-
-# TODO: document me!
-class _incremental_least_squares(object):
-    r"""
-    """
-    def __init__(self, l=0):
-        self.l = l
-
-    def train(self, X, Y):
-        # regularized least squares
-        XX = X.T.dot(X)
-        np.fill_diagonal(XX, self.l + np.diag(XX))
-        self.V = np.linalg.inv(XX)
-        self.W = self.V.dot(X.T.dot(Y))
-
-    def increment(self, X, Y):
-        # incremental regularized least squares
-        U = X.dot(self.V).dot(X.T)
-        np.fill_diagonal(U, 1 + np.diag(U))
-        U = np.linalg.inv(U)
-        Q = self.V.dot(X.T).dot(U).dot(X)
-        self.V = self.V - Q.dot(self.V)
-        self.W = self.W - Q.dot(self.W) + self.V.dot(X.T.dot(Y))
-
-    def predict(self, x):
-        return np.dot(x, self.W)
-
-
-# TODO: document me!
-class _incremental_indirect_least_squares(object):
-    r"""
-    """
-    def __init__(self, l=0, d=0):
-        self._ils = _incremental_least_squares(l)
-        self.d = d
-
-    def train(self, X, Y):
-        # regularized least squares exchanging the roles of X and Y
-        self._ils.train(Y, X)
-        J = self._ils.W
-        # solve the original problem by computing the pseudo-inverse of the
-        # previous solution
-        H = J.T.dot(J)
-        np.fill_diagonal(H, self.d + np.diag(H))
-        self.W = np.linalg.solve(H, J.T)
-
-    def predict(self, x):
-        return np.dot(x, self.W)
+        self._regressor_cls = incremental_indirect_least_squares
 
 
 # TODO: document me!
