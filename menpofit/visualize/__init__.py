87652723578f5f36e4387aedd3940b96395ecdb4
Epameinondas Antonakos <antonakosn@gmail.com>
2015-09-02 16:21:05 +0100

    small __init__.py change

--- menpofit/visualize/__init__.py
+++ menpofit/visualize/__init__.py
@@ -3,7 +3,7 @@ try:
     from .widgets import (visualize_shape_model, visualize_appearance_model,
                           visualize_patch_appearance_model, visualize_aam,
                           visualize_patch_aam, visualize_atm,
-                          visualize_patch_atm, plot_ced, 
+                          visualize_patch_atm, plot_ced,
                           visualize_fitting_result)
 except ImportError:
     pass
