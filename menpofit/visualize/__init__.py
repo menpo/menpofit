36a29edec23e2f1f9a1b5376432a224ce65c711b
Epameinondas Antonakos <antonakosn@gmail.com>
2016-02-18 18:01:36 +0000

    re-adds view_image_multiple_landmarks :)

--- menpofit/visualize/__init__.py
+++ menpofit/visualize/__init__.py
@@ -1,2 +1,3 @@
 from .textutils import print_progress, statistics_table
-from .base import plot_cumulative_error_distribution
+from .base import (view_image_multiple_landmarks,
+                   plot_cumulative_error_distribution)
