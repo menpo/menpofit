c380f0d1002336ec04a1bf8be134e5323b37a64d
Patrick Snape <patricksnape@gmail.com>
2016-03-04 14:36:06 +0000

    Add numpy as build requirement to conda

--- setup.py
+++ setup.py
@@ -3,27 +3,30 @@ import versioneer
 from Cython.Build import cythonize
 import numpy as np
 
+
 # ---- C/C++ EXTENSIONS ---- #
 cython_modules = ['menpofit/dpm/utils.pyx']
 
 cython_exts = cythonize(cython_modules, quiet=True)
 include_dirs = [np.get_include()]
 
+
 setup(
-      name='menpofit',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description="Menpo's image feature point localisation (AAMs, SDMs, "
-                  "CLMs, ERT, ASMs, APS, DPM)",
-      author='The Menpo Development Team',
-      author_email='james.booth08@imperial.ac.uk',
-      include_dirs=include_dirs,
-      ext_modules=cython_exts,
-      packages=find_packages(),
-      install_requires=['menpo>=0.6,<0.7',
-                        'scikit-learn>=0.17,<0.18',
-                        'Cython>=0.23,<0.24'],
-      package_data={'menpofit': ['dpm/cpp/*.h'],
-                    '': ['*.pxd', '*.pyx']},
-      tests_require=['nose', 'mock']
-      )
+     name='menpofit',
+     version=versioneer.get_version(),
+     cmdclass=versioneer.get_cmdclass(),
+     description="Menpo's image feature point localisation (AAMs, SDMs, "
+                 "CLMs, ERT, ASMs, APS, DPM)",
+     author='The Menpo Development Team',
+     author_email='james.booth08@imperial.ac.uk',
+     include_dirs=include_dirs,
+     ext_modules=cython_exts,
+     packages=find_packages(),
+     install_requires=['menpo>=0.6,<0.7',
+                       'scikit-learn>=0.17,<0.18',
+                       'numpy>=1.10,<1.11',
+                       'Cython>=0.23,<0.24'],
+     package_data={'menpofit': ['dpm/cpp/*.h'],
+                   '': ['*.pxd', '*.pyx']},
+     tests_require=['nose', 'mock']
+)
